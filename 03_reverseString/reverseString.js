3ba6add7a9ab560b3a6e76c50252c72ff64a6520
mrmasterb4 <140006398+mrmasterb4@users.noreply.github.com>
2024-01-17 06:11:38 -0800

    03reverse done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var length = string.length;
+    var reverse = "";
+    for (let i = length; i > -1; i--) {
+        reverse = reverse + string.charAt(i);
+    }
+    return reverse;
 };
 
+console.log(reverseString("hello"));
 // Do not edit below this line
 module.exports = reverseString;
