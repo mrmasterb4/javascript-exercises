026ddaeeb4dfde2c2bc699e3e17a152291c54ee1
mrmasterb4 <140006398+mrmasterb4@users.noreply.github.com>
2024-01-17 05:53:05 -0800

    02repeat done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    var result = "";
+    if (number < 0) {
+        return "ERROR"
+    }
+    else {
+        for (let i = 1; i <= number; i++) {
+            result = result + string
+        }
+        return result;
+    }
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
