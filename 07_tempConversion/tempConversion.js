9aa4e6b5d9addb4dfa32a20860d5802c0befbc5f
mrmasterb4 <140006398+mrmasterb4@users.noreply.github.com>
2024-01-17 07:36:27 -0800

    07-tempconversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  var celsius = (x - 32) * (5/9);
+  if (celsius % 1 == 0) {
+    return celsius;
+  }
+  else {
+    celsius = parseFloat(celsius.toFixed(1));
+    return celsius;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(y) {
+  var farhenheit = (y * (9/5) + 32);
+  if (farhenheit % 1 == 0) {
+    return farhenheit;
+  }
+  else {
+    farhenheit = parseFloat(farhenheit.toFixed(1));
+    return farhenheit;
+  }
 };
 
 // Do not edit below this line
