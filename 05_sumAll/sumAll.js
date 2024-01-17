53aee2ce82945fa66e52597df3dd7215b1bbef9c
mrmasterb4 <140006398+mrmasterb4@users.noreply.github.com>
2024-01-17 07:05:03 -0800

    05sumall done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    var sum = 0;
+    if (Number.isInteger(number1) && Number.isInteger(number2) && number1 >= 0 && number2 >= 0) {
+        if (number1 > number2) {
+            for (let i = number2; i <= number1; i++) {
+                sum = sum + i;
+            }
+        }
+        else {
+            for (let i = number1; i <= number2; i++) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
