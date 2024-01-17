6a48d4888626eebf29456c6bc2606f4160d51241
mrmasterb4 <140006398+mrmasterb4@users.noreply.github.com>
2024-01-17 06:56:14 -0800

    04removefromarray done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
 const removeFromArray = function() {
-
+    var array = arguments[0];
+    var arraylength = array.length;
+    var length = arguments.length - 1;
+    for (let i = 1; i <= length; i++) {
+        for (let p = 0; p <= arraylength; p++) {
+            var index = array.indexOf(arguments[i]);
+            if (index >= 0) {
+                array.splice(index, 1);
+            }
+        }
+    }
+    return array;
 };
 
+removeFromArray([1,2,3], 1, 2, 3);
 // Do not edit below this line
 module.exports = removeFromArray;
