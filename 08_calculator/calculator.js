ba61bd66eda4d055896812a21ae57165ad4d70db
mrmasterb4 <140006398+mrmasterb4@users.noreply.github.com>
2024-01-17 08:39:11 -0800

    08calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,69 @@
 const add = function() {
-	
+  var arglength = arguments.length
+  var sum = 0;
+  for (let i = 0; i <= arglength; i++) {
+    if (Number.isInteger(arguments[i])) {
+      sum = sum + arguments[i];
+    }
+  }
+  return sum;
 };
 
 const subtract = function() {
-	
+  var arglength = arguments.length
+  var sum = 0;
+  for (let i = 0; i <= arglength; i++) {
+    if (Number.isInteger(arguments[i])) {
+      if (sum == 0) {
+        sum = ((sum - arguments[i]) * -1);
+      }
+      else {
+        sum = sum - arguments[i];
+      }
+    }
+  }
+  return sum;
 };
 
 const sum = function() {
-	
+	var array = arguments[0];
+  var arrLength = array.length;
+  var sum = 0;
+  for (let i = 0; i <= arrLength; i++) {
+    if (Number.isInteger(array[i])) {
+      sum = sum + array[i];
+    }
+  }
+  return sum;
 };
 
 const multiply = function() {
-
+	var array = arguments[0];
+  var arrLength = array.length;
+  var sum = 0;
+  for (let i = 0; i <= arrLength; i++) {
+    if (Number.isInteger(array[i])) {
+      if (sum == 0) {
+        sum = (sum + array[i]);
+      }
+      else {
+        sum = sum * array[i];
+      }
+    }
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y)
 };
 
-const factorial = function() {
+const factorial = function(x) {
+  var factorial = 1;
+  for (let i = 1; i <= x; i++) {
+    factorial = factorial * i;
+  }
+  return factorial;
 	
 };
 
