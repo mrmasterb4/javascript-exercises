e60deed3124eca31117b8efe4442e87bd564ad37
mrmasterb4 <140006398+mrmasterb4@users.noreply.github.com>
2024-01-18 09:43:07 -0800

    10_fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    const sums = [0,1,1];
+    var result = 0;
+    position = parseInt(position);
+    if (position >= 0) {
+        for (let i = 1; i <= position + 3; i++) {
+            result = sums[i] +  sums[i + 1];
+            sums.push(result);
+        }
+        console.log(sums[position]);
+        return sums[position];
+    }
+    else {
+        return "OOPS";
+    }
 };
-
+fibonacci(0);
 // Do not edit below this line
 module.exports = fibonacci;
