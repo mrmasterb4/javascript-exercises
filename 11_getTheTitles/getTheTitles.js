dfe9c7c56cb799bcdd553f676b2d6ac613d8bd1e
mrmasterb4 <140006398+mrmasterb4@users.noreply.github.com>
2024-01-18 10:23:54 -0800

    11 savethetitles done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.map((item) => item.title); // each item is {title: x, author: x} so if we say return item where => item.title it will return value of item,/
 };
 
 // Do not edit below this line
