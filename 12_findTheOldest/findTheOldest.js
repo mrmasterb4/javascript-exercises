7bb205e3d0ae518a81c0d76dbad0385f89b67565
mrmasterb4 <140006398+mrmasterb4@users.noreply.github.com>
2024-01-20 12:46:38 -0800

    12-findoldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,45 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    var currentTime = new Date()
+    var oldestalive = [];
+    var oldestdead = [];
+    array.reduce((accumulator, currentValue) => {
+        if (!getDeath(currentValue.yearOfDeath, currentValue.yearOfBirth)) {
+            if (oldestalive.length == 0) {
+                oldestalive = currentValue;
+            }
+            else if ((currentTime.getFullYear() - currentValue.yearOfBirth) > (currentTime.getFullYear() - oldestalive.yearOfBirth)) {
+                oldestalive = currentValue;
+            }
+        }
+        else {
+            if (oldestdead.length == 0) {
+                oldestdead = currentValue;
+            }
+            else if ((currentValue.yearOfDeath - currentValue.yearOfBirth) > (oldestdead.yearOfDeath - oldestdead.yearOfBirth)) {
+                oldestdead = currentValue;
+            }
+        }
+        accumulator = oldestdead;
+        console.log("Current", accumulator);
+    }, 0);
+    if (oldestalive.length == 0) {
+        return oldestdead;
+    }
+    else if ((currentTime.getFullYear() - oldestalive.yearOfBirth) < (oldestdead.yearOfDeath - oldestdead.yearOfBirth)) {
+        return oldestdead;
+    }
+    else {
+        return oldestalive;
+    }
 };
 
+function getDeath(dead, alive) {
+    if(!dead) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
 // Do not edit below this line
 module.exports = findTheOldest;
